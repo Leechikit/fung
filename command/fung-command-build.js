d2a3c882ee208db4a643172b6ce608463637b1fd
李子杰 <leechikit0823@163.com>
2017-09-30 18:01:59 +0800

    update

--- command/fung-command-build.js
+++ command/fung-command-build.js
@@ -17,47 +17,53 @@ exports.register = function (commander) {
         .option('-r, --remote [repertory]', '远程仓库')
         .description('构建项目')
         .action(option => {
-            exec('git branch -r', { cwd: gitDir }, (err, stdout, stderr) => {
+            exec('git remote update', { cwd: gitDir }, (err, stdout, stderr) => {
                 if (err) throw err;
-                let repertory = option.remote || fs.readFileSync(repertoryFile, 'utf8');
-                let list = String.prototype.split.call(stdout, '\n');
-                let choices = [];
-                let promps = [];
-                list = _.chain(list)
-                    .map(item => {
-                        return item.replace(/.*origin\/(\S+)/, "$1");
-                    })
-                    .filter(item => {
-                        return _.trim(item) != '' && item != 'master';
-                    })
-                    .uniq()
-                    .value();
-                // 参数设置的分支存在
-                if (list.indexOf(option.branch) > -1) {
-                    pullBranch({
-                        repertory,
-                        branch: option.branch
-                    }).then(() => {
-                        console.log(`${option.branch} 构建成功`);
+                exec('git remote prune origin', { cwd: gitDir }, (err, stdout, stderr) => {
+                    if (err) throw err;
+                    exec('git branch -r', { cwd: gitDir }, (err, stdout, stderr) => {
+                        if (err) throw err;
+                        let repertory = option.remote || fs.readFileSync(repertoryFile, 'utf8');
+                        let list = String.prototype.split.call(stdout, '\n');
+                        let choices = [];
+                        let promps = [];
+                        list = _.chain(list)
+                            .map(item => {
+                                return item.replace(/.*origin\/(\S+)/, "$1");
+                            })
+                            .filter(item => {
+                                return _.trim(item) != '' && item != 'master';
+                            })
+                            .uniq()
+                            .value();
+                        // 参数设置的分支存在
+                        if (list.indexOf(option.branch) > -1) {
+                            pullBranch({
+                                repertory,
+                                branch: option.branch
+                            }).then(() => {
+                                copyFolder(gitDir, currDir);
+                                console.log(`${option.branch} 构建成功`);
+                            });
+                        } else {
+                            promps.push({
+                                type: 'list',
+                                name: 'branches',
+                                message: '请选择构建的分支',
+                                choices: _.map(list, item => { return { name: item, value: item } })
+                            })
+                        }
+                        promps.length > 0 && inquirer.prompt(promps).then(function (answers) {
+                            pullBranch({
+                                repertory,
+                                branch: answers.branches
+                            }).then(() => {
+                                copyFolder(gitDir, currDir);
+                                console.log(`${answers.branches} 构建成功`);
+                            });
+                        })
                     });
-                } else {
-                    promps.push({
-                        type: 'list',
-                        name: 'branches',
-                        message: '请选择构建的分支',
-                        choices: _.map(list, item => { return { name: item, value: item } })
-                    })
-                }
-                promps.length > 0 && inquirer.prompt(promps).then(function (answers) {
-                    pullBranch({
-                        repertory,
-                        branch: answers.branches
-                    }).then(() => {
-                        console.log(`${answers.branches} 构建成功`);
-                    });
-
-                    copyFolder(gitDir, currDir);
-                })
-            })
+                });
+            });
         })
 }
