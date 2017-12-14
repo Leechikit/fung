278abb64ee188f50af0d5ded0ebbb1aa449a119c
李子杰 <leechikit0823@163.com>
2017-12-14 18:34:45 +0800

    update

---
+++ test/replace.test.js
@@ -0,0 +1,32 @@
+const replace = require('../lib/replace.js');
+const expect = require('chai').expect;
+const config = {
+    apple: 1,
+    orange: 2,
+}
+
+describe('test replace function', function () {
+    it('normal situation', function () {
+        const template = '<div>{{apple}}</div>';
+        expect(replace(template, config)).to.be.equal('<div>1</div>');
+    });
+
+    it('multiple replace situation', function () {
+        const template = 
+        `<div>{{apple}}</div>
+        <div>{{orange}}</div>`;
+        expect(replace(template, config)).to.be.equal(
+        `<div>1</div>
+        <div>2</div>`);
+    });
+
+    it('not standard template situation', function () {
+        const template = '<div>{{ apple      }}</div>';
+        expect(replace(template, config)).to.be.equal('<div>1</div>');
+    });
+
+    it('template is not match with config', function(){
+        const template = '<div>{{sheep}}</div>';
+        expect(replace(template, config)).to.be.equal('<div>{{sheep}}</div>');
+    });
+});
