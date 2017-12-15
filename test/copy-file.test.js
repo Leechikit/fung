6fcd029a3bd276b43fd91a8d09b728d03b674217
李子杰 <leechikit0823@163.com>
2017-12-15 17:53:41 +0800

    update

---
+++ test/copy-file.test.js
@@ -0,0 +1,11 @@
+const replace = require('../lib/copy-file.js');
+const expect = require('chai').expect;
+const srcPath = '';
+const tarPath = '';
+const config = {}
+
+described('test copy file function', function () {
+    it('', function () {
+
+    })
+});
