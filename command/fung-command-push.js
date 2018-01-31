b3396edb7b852fbdbfde8568f4196914102197b1
leechikit <leechikit0823@163.com>
2018-01-31 21:22:21 +0800

    update

--- command/fung-command-push.js
+++ command/fung-command-push.js
@@ -17,6 +17,12 @@ exports.register = function (commander) {
         .action(option => {
             let repertory = option.remote || fs.readFileSync(repertoryFile, 'utf8');
             let branchName = option.branch || path.basename(currDir);
+            // 在本地创建远程追踪分支
+            // await exec('git remote update');
+            // 删除本地版本库上那些失效的远程追踪分支
+            // await exec('git remote prune origin');
+            // 列出所有被跟踪的远程分支
+            // let stdout = await exec('git branch -r');
             exec('git remote update')
                 .then(exec.bind(null, 'git remote prune origin'))
                 .then(exec.bind(null, 'git branch -r'))
