1407252095c379cde52d514390528529bd7c81e8
leechikit <leechikit0823@163.com>
2018-04-10 15:17:10 +0800

    update

--- bin/fung.js
+++ bin/fung.js
@@ -11,7 +11,7 @@ command_list.register(commander);
 command_build.register(commander);
 
 commander
-    .version('0.2.0')
+    .version('0.2.1')
     .parse(process.argv);
 
 process.on('unhandledRejection', err => {
